3c78eb984db74febcd4efaa1136a0021490e8704
Reese Armstrong <me@reeseric.ci>
2022-08-19 18:29:37 -0500

    Update nav.svelte

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -25,15 +25,18 @@
         align-items: center;
         z-index: 1;
     }
+    
+     a {
+        color: white;
+        text-decoration: none;
+        font-weight: bold;
+    }
 
     a:not(.button > a) {
         box-shadow: inset 0 0 0 0 var(--primary-green);
-        color: white;
         margin: 0 -.25rem;
         padding: 0 .25rem;
         transition: color .3s ease-in-out, box-shadow .3s ease-in-out;
-        text-decoration: none;
-        font-weight: bold;
     }
     a:not(.button > a):hover {
         box-shadow: inset 100px 0 0 0 var(--primary-green);
